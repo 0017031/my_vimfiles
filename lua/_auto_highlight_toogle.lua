06c045951a74e0c56c35a0e78c0443ec080a8d27
Baichun <baic@sha157104>
2024-01-19 15:17:40 +0800

    _auto_highlight_toogle.lua, rename augrp name

--- lua/_auto_highlight_toogle.lua
+++ lua/_auto_highlight_toogle.lua
@@ -1,16 +1,16 @@
 function my_Auto_Highlight_Toggle()
 	if vim.g.ah2 then
-		local _, _= pcall(function() vim.api.nvim_clear_autocmds({group = "ah2"}) end)
-		vim.opt_local.updatetime=800; vim.fn.setreg('/', '');  vim.opt_local.hlsearch=false
-		vim.g.ah2=false; print "2Highlight current word: off"
+		local _, _ = pcall(function() vim.api.nvim_clear_autocmds({ group = "my_Auto_Highlight_augrp" }) end)
+		vim.opt_local.updatetime = 800; vim.fn.setreg('/', ''); vim.opt_local.hlsearch = false
+		vim.g.ah2 = false; print "2Highlight current word: off"
 		return false
 	else
-		local ah2 = vim.api.nvim_create_augroup('ah2', { clear = true })
-		-- au CursorHold * let @/ = '\V\<'.escape(expand('<cword>'), '\').'\>'
-		vim.api.nvim_create_autocmd("CursorHold", { command = "let @/ = '\\V\\<'.escape(expand('<cword>'), '\\').'\\>'" , group = ah2, })
-		vim.opt_local.updatetime=300;  vim.opt_local.hlsearch=true
-		vim.g.ah2=true; print "2Highlight current word: ON"
+		vim.api.nvim_create_autocmd("CursorHold", {
+			group = vim.api.nvim_create_augroup('my_Auto_Highlight_augrp', { clear = true }),
+			command = "let @/ = '\\V\\<'.escape(expand('<cword>'), '\\').'\\>'",
+		})
+		vim.opt_local.updatetime = 300; vim.opt_local.hlsearch = true
+		vim.g.ah2 = true; print "2Highlight current word: ON"
 		return true
 	end
 end
-
